9905ec0533ed4b922683062adb5f088889348527
snowspark7891 <someshkumar7891@gmail.com>
2025-01-02 12:56:48 +0530

    recheck

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -14,20 +14,3 @@ interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
 
-// /// <reference types="vite/client" />
-
-// interface ImportMetaEnv {
-// //   readonly VITE_API_KEY: string;
-//   readonly VITE_APPWRITE_PROJECTID: string;
-//   readonly VITE_APPWRITE_URL: string;
-//   readonly VITE_APPWRITE_DATABASE_ID: string;
-//   readonly VITE_APPWRITE_MEDIA_ID: string;
-//   readonly VITE_APPWRITE_USERS: string;
-//   readonly VITE_APPWRITE_POSTS: string;
-//   readonly VITE_APPWRITE_SAVES: string;
-//   // Add other environment variables here as needed
-// }
-
-// interface ImportMeta {
-//   readonly env: ImportMetaEnv;
-// }
